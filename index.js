27801babde1c81a3dd1798ab40831cbd35f1d56e
Muhammad Ibrahim <genecod9@gmail.com>
2026-08-05 02:09:10 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
+app.use(cors());
 const snapsave = require("./snapsave-downloader/src/index");
 const port = 3000;
 
