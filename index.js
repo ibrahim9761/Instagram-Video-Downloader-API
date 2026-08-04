9ec52318dff232368f514b9150ac77bd0c342129
Muhammad Ibrahim <genecod9@gmail.com>
2026-08-05 02:09:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,5 @@ app.get("/igdl", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('running on ' + PORT));
